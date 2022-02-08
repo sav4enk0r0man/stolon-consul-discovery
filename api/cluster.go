c557cafc4f424cb5a33fc8bc80e649b5bc549436
Savchenko Roman <r.savchenko@corp.mail.ru>
2022-02-08 18:00:59 +0300

    add cluster api

--- api/cluster.go
+++ api/cluster.go
@@ -5,21 +5,57 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/sav4enk0r0man/stolon-consul-discovery/client"
+	"github.com/sav4enk0r0man/stolon-consul-discovery/logger"
 )
 
-func ClusterState(clusterName string, url string) string {
+var (
+	logError = logger.DefaultLog.Error
+)
+
+func GetClusterData(ctx context) ([]Index, error) {
+	clusterName := ctx.GetConf("cluster")
+	url := ctx.GetConf("url")
+
 	api := fmt.Sprintf("%s/v1/kv/stolon/cluster/%s/clusterdata", url, clusterName)
-	response := client.ConsulClient(api)
+	response, _ := client.Get(api, map[string]string{
+		"httptimeout": ctx.GetConf("httptimeout"),
+	})
 
 	indexes := make([]Index, 0)
 	err := json.Unmarshal(response, &indexes)
 	if err != nil {
-		fmt.Printf("unmarshal error: %s", err)
+		return nil, logger.Wrapper(err, err.Error())
+	}
+	return indexes, nil
+}
+
+func ShowClusterData(ctx context) (string, error) {
+	indexes, err := GetClusterData(ctx)
+	if err != nil {
+		return "", err
 	}
 
-	decoded, err := b64.URLEncoding.DecodeString(string(indexes[0].Value))
+	data, err := json.MarshalIndent(indexes, "", "    ")
 	if err != nil {
-		fmt.Printf("decode error: %s", err)
+		return "", logger.Wrapper(err, err.Error())
 	}
-	return string(decoded)
+	resp := fmt.Sprintf("Cluster raw data: %s\n", string(data))
+
+	decodedValue, err := b64.URLEncoding.DecodeString(string(indexes[0].Value))
+	if err != nil {
+		return "", logger.Wrapper(err, err.Error())
+	}
+
+	var value ClusterData
+	if err = json.Unmarshal([]byte(decodedValue), &value); err != nil {
+		return "", logger.Wrapper(err, err.Error())
+	}
+
+	data, err = json.MarshalIndent(value, "", "    ")
+	if err != nil {
+		return "", logger.Wrapper(err, err.Error())
+	}
+
+	resp += fmt.Sprintf("\nCluster data value: %s\n", string(data))
+	return resp, nil
 }
